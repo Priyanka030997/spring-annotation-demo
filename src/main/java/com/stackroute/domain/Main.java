8cafc1829061bb0591545a1e6ee7c00d570a3336
priyanka <priyanka150skct@gmail.com>
2019-07-14 19:44:23 +0530

    completed task3 using annotation

--- src/main/java/com/stackroute/domain/Main.java
+++ src/main/java/com/stackroute/domain/Main.java
@@ -13,17 +13,9 @@ public static void main(String args[])
 {
     //use ApplicationContext
     ApplicationContext context=new AnnotationConfigApplicationContext(AppConfig.class);
-   // Movie movieA=context.getBean("movieBean1",Movie.class);
-   //movieA.display();
-    Movie movieA1=context.getBean("movieBeanA",Movie.class);
-    movieA1.display();
-    Movie movieA2=context.getBean("movieBeanB",Movie.class);
-    movieA2.display();
-    Movie movieB=context.getBean("movieBean2",Movie.class);
-    movieB.display();
-    Movie movieC=context.getBean("movieBean3",Movie.class);
-    movieC.display();
-    Movie movieD=context.getBean("movieBean3",Movie.class);
-    System.out.println(movieC==movieD);
+    Movie movieA=context.getBean("movieBean1",Movie.class);
+   movieA.display();
+
+
 }
 }
